9f01d2c2414870c1877d9a489e16e9601aec2637
tim-zab <rga-1971@mail.ru>
2025-05-12 10:36:17 +0300

    update project

--- src/test/java/rga/users/subscriptions/management/app/ApplicationTest.java
+++ src/test/java/rga/users/subscriptions/management/app/ApplicationTest.java
@@ -2,7 +2,11 @@ package rga.users.subscriptions.management.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+@ActiveProfiles("test")
+@TestPropertySource(locations = "/application-test.yml")
 @SpringBootTest
 class ApplicationTest {
 
